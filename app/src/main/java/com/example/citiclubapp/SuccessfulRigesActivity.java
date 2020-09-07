20101965e5d98ac93f74b3f8a3b48866823d9981
吴瀚之 <whzzshhh@gmail.com>
2020-09-07 17:01:31 +0800

    仓单宝1.0 完成了三个页面的布置，分别是启动页面，登录界面，注册界面，但是页面间的串联还没有完全的完成，页面中的蓝色按钮，灰色按钮，文本框的形式都已经设置好放在了drawable和style中，花UI的时候直接调用即可

--- app/src/main/java/com/example/citiclubapp/SuccessfulRigesActivity.java
+++ app/src/main/java/com/example/citiclubapp/SuccessfulRigesActivity.java
@@ -3,6 +3,7 @@ package com.example.citiclubapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SuccessfulRigesActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class SuccessfulRigesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successful_riges);
+        Button button;
     }
 }
