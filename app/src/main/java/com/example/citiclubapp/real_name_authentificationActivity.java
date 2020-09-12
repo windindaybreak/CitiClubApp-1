76d60485e9c0bc5e64a893aee311727b0f4c4470
吴瀚之 <whzzshhh@gmail.com>
2020-09-12 20:17:49 +0800

    仓单宝1.4 添加了4个底部导航的部件，初步画出底部导航的结构

---
+++ app/src/main/java/com/example/citiclubapp/real_name_authentificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.citiclubapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class real_name_authentificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real_name_authentification);
+    }
+}
